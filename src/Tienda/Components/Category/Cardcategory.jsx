0259df8b59e18c903f536a9066fc17f4ed927be4
jherry-Ponce <jherry_libra23@hotmail.com>
2022-07-17 18:00:22 -0500

    responsive

--- src/Tienda/Components/Category/Cardcategory.jsx
+++ src/Tienda/Components/Category/Cardcategory.jsx
@@ -4,13 +4,13 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 export const Cardcategory=()=>{
     return(
         <Row>
-        <Col lg={12}> <h3>CELULARES Y SMARTPHONES / SAMSUNG</h3></Col>
         <Col lg={12}>
         <Row className="border align-items-center shadow-sm bg-white px-0">
             <Col lg={12} className=" px-0">
                 <img src="https://i.linio.com/cms/1f215c6e-8489-11ea-9caa-ca3fed3fd0ef.webp" alt="" className='w-100 banner-category ' />    
             </Col>
-            <Col lg={6} md={8} sm={8} xs={8} >
+            <Col lg={6}> <h4>CELULARES Y SMARTPHONES / SAMSUNG</h4></Col>
+            <Col lg={4} md={8} sm={8} xs={8} >
                 <Row>
                     <Col sm={3} xs={3} className=""> 
                     <NavDropdown title="Marca" id="navbarScrollingDropdown">
@@ -53,22 +53,9 @@ export const Cardcategory=()=>{
                     </Col>
                 </Row>
             </Col>
-            <Col lg={6} md={4} sm={4} xs={4}>
+            <Col lg={2} md={4} sm={4} xs={4}>
                 <Row className="align-items-center">
-                    <Col xs={6} className="d-flex justify-content-end" > 
-                        <NavDropdown title="Marca" id="navbarScrollingDropdown">
-                        <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action4">
-                            Another action
-                        </NavDropdown.Item>
-                        {/* muestra la line horizontal */}
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action5">
-                            Something else here
-                        </NavDropdown.Item>
-                        </NavDropdown>
-                    </Col>
-                    <Col  xs={6} className=" d-flex justify-content-end ">
+                    <Col  xs={12} className=" d-flex justify-content-end ">
                         <span className='me-3 cursor-pointer'>
                             <i className="fa fa-th-large fa-2x " aria-hidden="true"></i>
                         </span>
